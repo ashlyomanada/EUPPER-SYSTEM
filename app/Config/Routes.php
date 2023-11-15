415e792f3112ce8a468ad1b6cde5fa93c544d360
ashlyomanada <128467630+ashlyomanada@users.noreply.github.com>
2023-11-15 20:45:59 +0800

    backend codes

---
+++ app/Config/Routes.php
@@ -0,0 +1,14 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'MainController::index');
+$routes->post('/save', 'MainController::save');
+$routes->get('/getUsers', 'MainController::getUsers');
+$routes->get('/getAdmins', 'MainController::getAdmins');
+$routes->get('/getPpo', 'MainController::getPpo');
+$routes->post('/login', 'MainController::login');
+$routes->post('/generate', 'MainController::generateExcel');
